85fe6100f0f4566b14e3dccb40eaf0a11d1eb79c
Mayor <akintayomayomiolamide@gmail.com>
2022-12-07 15:52:17 -0800

    python

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def sqaure_matrix_simple(matrix=[]):
+def square_matrix_simple(matrix=[]):
     tmp = []
     for x in matrix:
-        tmp.append(list(map(lamda x: x**2, x)))
+        tmp.append(list(map(lambda x: x**2, x)))
     return (tmp)
