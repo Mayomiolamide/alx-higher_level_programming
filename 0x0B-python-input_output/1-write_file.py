96f5cc1aebd183d48265aaac65d5e0341b21a512
Mayomiolamide <akintayomayomiolamide@gmail.com>
2023-01-11 04:39:48 -0800

    py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,47 +1,20 @@
 #!/usr/bin/python3
-
 """Module 1-number_of_lines
-
 Counts number of ;lines in a file
-
 """
 
-
-
-
-
 def number_of_lines(filename=""):
-
-    """Counts lines in filename
-
-
-
+    """Counts lines in filenam
     Args:
-
-        - filename: name of the file
-
-
-
+        - filename: name of te file
     Returns:
-
         - number of lines
 
     """
-
-
-
     count = 0
-
-
-
     with open(filename) as f:
-
         text = f.readlines()
-
         for line in text:
-
             count += 1
-
-
 
     return count
